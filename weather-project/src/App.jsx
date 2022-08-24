4c82f3a41d5eb06fed9852b145c8cf8278bdf3a7
João Antônio <101114950+B-e-sa@users.noreply.github.com>
2022-08-24 11:58:53 -0300

    Implementing axios and updating READ.ME instructions

--- weather-project/src/App.jsx
+++ weather-project/src/App.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Info from './components/info-container/Info'
 import Loading from './components/loading-container/Loading'
 import React, { useState } from 'react'
+import axios from 'axios'
 
 function App() {
 
@@ -21,17 +22,14 @@ function App() {
 
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric&lang=${language}`
 
-      fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
+      axios.get(url)
+        .then((res) => {
           setWeather({
-            main: data.main,
-            local: data.name,
-            temperature: data.main.temp,
-            clouds: data.weather[0].description,
-            humidity: data.main.humidity,
-            wind: data.wind.speed
-
+            main: res.data.main,
+            local: res.data.name,
+            temperature: res.data.main.temp,
+            clouds: res.data.weather[0].description,
+            wind: res.data.wind.speed
           })
           setLocation('')
         })
